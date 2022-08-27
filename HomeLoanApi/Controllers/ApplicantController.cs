907b77c0429d806aedeed8cd99d4322abe5810c1
Raghav Ambashta <raghav.ambashta22@gmail.com>
2022-08-28 02:29:53 +0530

    Add project files.

---
+++ HomeLoanApi/Controllers/ApplicantController.cs
@@ -0,0 +1,73 @@
+﻿using HomeLoanApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+namespace HomeLoanApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicantController : ControllerBase
+    {
+        HomeLoanContext db = new HomeLoanContext();
+        [HttpGet]
+        [Route("ListApplicant")]
+        public IActionResult GetApplicant()
+        {
+            var data = from Applicant in db.Applicants
+                       select new
+                       {
+                           UId = Applicant.UId,
+                           FirstName = Applicant.FirstName,
+                           MiddleName = Applicant.MiddleName,
+                           LastName = Applicant.LastName,
+                           Dob = Applicant.Dob,
+                           Gender = Applicant.Gender,
+                           Nationality = Applicant.Nationality,
+                           AadharNum = Applicant.AadharNum,
+                           PanNum = Applicant.PanNum,
+                           CId = Applicant.CId
+                       };
+            return Ok(data);
+        }
+        [HttpPost]
+        [Route("AddApplicant")]
+        public IActionResult PostApplicant(Applicant applicant)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    db.Applicants.Add(applicant);
+                    db.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.InnerException.Message);
+                }
+            }
+            return Created("Record Successfully Added", applicant);
+        }
+
+        [HttpPut]
+        [Route("EditApplicant/{id}")]
+        public IActionResult PutCred(int id, Applicant applicant)
+        {
+            if (ModelState.IsValid)
+            {
+                Applicant oapplicant = db.Applicants.Find(id);
+                oapplicant.FirstName = applicant.FirstName;
+                oapplicant.MiddleName = applicant.MiddleName;
+                oapplicant.LastName = applicant.LastName;
+                oapplicant.Dob = applicant.Dob;
+                oapplicant.Gender = applicant.Gender;
+                oapplicant.Nationality = applicant.Nationality;
+                oapplicant.AadharNum = applicant.AadharNum;
+                oapplicant.PanNum = applicant.PanNum;
+                db.SaveChanges();
+                return Ok();
+            }
+            return BadRequest("Unable to edit record");
+        }
+    }
+}
---
+++ HomeLoanApi/Models/Doc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace HomeLoanApi.Models
+{
+    public partial class Doc
+    {
+        public int DId { get; set; }
+        public byte[] Aadhaar { get; set; }
+        public byte[] Pan { get; set; }
+        public byte[] Voter { get; set; }
+        public byte[] SalarySlip { get; set; }
+        public byte[] Loa { get; set; }
+        public byte[] Noc { get; set; }
+        public byte[] Agreement { get; set; }
+        public int CId { get; set; }
+
+        public virtual Cred CIdNavigation { get; set; }
+    }
+}
